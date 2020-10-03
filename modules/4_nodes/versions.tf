503215865b0cb78a4d6a3de32ef36daecc325fef
Yussuf Shaikh <17543270+yussufsh@users.noreply.github.com>
2020-10-03 10:53:07 +0000

    Use the latest ibm provider plugin v1.13.0
    Signed-off-by: Yussuf Shaikh <17543270+yussufsh@users.noreply.github.com>

--- modules/4_nodes/versions.tf
+++ modules/4_nodes/versions.tf
@@ -22,7 +22,7 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-      version = "~> 1.9.0"
+      version = "~> 1.13.0"
     }
     ignition = {
       source  = "terraform-providers/ignition"
