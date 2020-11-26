bc807c7f17fbf1e4b35c07567383a2f53c7fbbd5
Yussuf Shaikh <yussuf.shaikh@ibm.com>
2020-11-26 13:21:30 +0000

    Update IBM provider to the latest version
    The latest version has couple of fixes for PI instance
    1. https://github.com/IBM-Cloud/terraform-provider-ibm/issues/1930
    2. https://github.com/IBM-Cloud/terraform-provider-ibm/issues/2046
    
    Signed-off-by: Yussuf Shaikh <yussuf.shaikh@ibm.com>

--- modules/1_prepare/versions.tf
+++ modules/1_prepare/versions.tf
@@ -22,7 +22,7 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-      version = "~> 1.13.0"
+      version = "~> 1.15.0"
     }
     null = {
       source  = "hashicorp/null"
